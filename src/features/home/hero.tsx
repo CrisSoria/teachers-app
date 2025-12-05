70ba56be00d464973149b262d4e64c62177409be
Cristian Soria <crissoria07@gmail.com>
2025-12-05 01:27:02 -0300

    fix tailwind class v4

--- src/features/home/hero.tsx
+++ src/features/home/hero.tsx
@@ -14,7 +14,7 @@ export function Hero() {
           <img
             src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=200"
             alt="user3"
-            className="size-8 object-cover rounded-full border-2 border-white hover:-translate-y-0.5 transition z-[1]"
+            className="size-8 object-cover rounded-full border-2 border-white hover:-translate-y-0.5 transition z-1"
           />
           <img
             src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200"
@@ -24,17 +24,17 @@ export function Hero() {
           <img
             src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=200"
             alt="user2"
-            className="size-8 object-cover rounded-full border-2 border-white hover:-translate-y-0.5 transition z-[3]"
+            className="size-8 object-cover rounded-full border-2 border-white hover:-translate-y-0.5 transition z-3"
           />
           <img
             src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=200"
             alt="user3"
-            className="size-8 object-cover rounded-full border-2 border-white hover:-translate-y-0.5 transition z-[4]"
+            className="size-8 object-cover rounded-full border-2 border-white hover:-translate-y-0.5 transition z-4"
           />
           <img
             src="https://randomuser.me/api/portraits/men/75.jpg"
             alt="user5"
-            className="size-8 rounded-full border-2 border-white hover:-translate-y-0.5 transition z-[5]"
+            className="size-8 rounded-full border-2 border-white hover:-translate-y-0.5 transition z-5"
           />
         </div>
 
